9bf501758ea725f24d945e7ddb0c868c7f0e6e36
matmiranda <mat.miranda@outlook.com>
2025-07-21 12:31:27 -0300

    ajsutes

--- MenuProducerService.Application/DTOs/MenuItemRequest.cs
+++ MenuProducerService.Application/DTOs/MenuItemRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace MenuProducerService.Application.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MenuProducerService.Application.DTOs
 {
     public class MenuItemRequest
     {
@@ -6,6 +8,8 @@
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
+        [Required(ErrorMessage = "O tipo de refeição é obrigatório.")]
+        [RegularExpression("LANCHES|SOBREMESAS|BEBIDAS", ErrorMessage = "MealType deve ser LANCHES, SOBREMESAS ou BEBIDAS.")]
         public string MealType { get; set; } = string.Empty;
         public bool Available { get; set; } = true;
         public string Action { get; set; } = "CREATE"; // CREATE or UPDATE
